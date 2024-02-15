1f1fd6f7c6f8e5d605bfef6b2fb7760d5052a071
Alyx <github@alyx.pink>
2024-02-15 15:18:15 +0100

    Add basic characters and resources management

--- src/server/server.go
+++ src/server/server.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
-	pb "github.com/VoidMesh/backend/src/api/v1"
 	"log"
 	"net"
+
+	pb "github.com/VoidMesh/backend/src/api/v1"
+	"github.com/google/uuid"
 
 	"google.golang.org/grpc"
 )
@@ -12,6 +14,15 @@ import (
 // server is used to implement greeter.GreeterServer.
 type server struct {
 	pb.UnimplementedGreeterServer
+	pb.UnimplementedCharacterServiceServer
+	pb.UnimplementedResourceServiceServer
+}
+
+var Characters = map[string]*pb.Character{}
+
+var Resources = []*pb.Resource{
+	{Id: "1", Name: "Tritium", Description: "A rare and valuable resource."},
+	{Id: "2", Name: "Titanium", Description: "A common and useful resource."},
 }
 
 // SayHello implements greeter.GreeterServer
@@ -20,13 +31,37 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
 
+func (s *server) CreateCharacter(ctx context.Context, in *pb.CreateCharacterRequest) (*pb.CreateCharacterResponse, error) {
+	log.Printf("Received: %v", in.GetCharacter())
+	log.Printf("Creating character: %v", in.Character.Name)
+	in.Character.Id = uuid.New().String()
+	Characters[in.Character.Name] = in.Character
+	return &pb.CreateCharacterResponse{Character: Characters[in.Character.Name]}, nil
+}
+
+func (s *server) ReadCharacter(ctx context.Context, in *pb.ReadCharacterRequest) (*pb.ReadCharacterResponse, error) {
+	log.Printf("Received: %v", in.GetCharacter())
+	log.Printf("Getting character named: %v", in.Character.Name)
+	return &pb.ReadCharacterResponse{Character: Characters[in.Character.Name]}, nil
+}
+
+func (s *server) ListResources(ctx context.Context, in *pb.ListResourcesRequest) (*pb.ListResourcesResponse, error) {
+	log.Printf("Received: %v", in)
+	log.Printf("Listing resources: %v", Resources)
+	return &pb.ListResourcesResponse{Resources: Resources}, nil
+}
+
 func main() {
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
+	pb.RegisterCharacterServiceServer(s, &server{})
+	pb.RegisterResourceServiceServer(s, &server{})
+
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
