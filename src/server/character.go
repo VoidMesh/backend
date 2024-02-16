a16eb2f2e172a500015fe294af3642a97cb0d72e
Victor Bersy <victor.bersy@hey.com>
2024-02-16 03:18:22 +0100

    Continue making step forwards

--- src/server/character.go
+++ src/server/character.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math/rand"
 
 	"github.com/VoidMesh/backend/src/api/v1/character"
 	"github.com/google/uuid"
@@ -17,13 +19,34 @@ type CharacterServer struct {
 func (s *CharacterServer) Create(ctx context.Context, in *character.CreateRequest) (*character.CreateResponse, error) {
 	log.Printf("Received: %v", in.GetCharacter())
 	log.Printf("Creating character: %v", in.Character.Name)
+
 	in.Character.Id = uuid.New().String()
-	Characters[in.Character.Name] = in.Character
-	return &character.CreateResponse{Character: Characters[in.Character.Name]}, nil
+	in.Character.Inventory = &character.Inventory{
+		Resources: make(map[string]int64),
+	}
+	Characters[in.Character.Id] = in.Character
+
+	return &character.CreateResponse{Character: Characters[in.Character.Id]}, nil
 }
 
 func (s *CharacterServer) Read(ctx context.Context, in *character.ReadRequest) (*character.ReadResponse, error) {
-	log.Printf("Received: %v", in.GetCharacter())
-	log.Printf("Getting character named: %v", in.Character.Name)
-	return &character.ReadResponse{Character: Characters[in.Character.Name]}, nil
+	log.Printf("Received character ID: %s", in.Id)
+
+	c := Characters[in.Id]
+
+	if c == nil {
+		return nil, fmt.Errorf("Character not found")
+	}
+
+	return &character.ReadResponse{Character: c}, nil
+}
+
+func (s *CharacterServer) GatherResource(ctx context.Context, in *character.GatherResourceRequest) (*character.GatherResourceResponse, error) {
+	c := Characters[in.CharacterId]
+
+	fmt.Printf("Gathering resource for character: %v", c)
+
+	c.Inventory.Resources[in.ResourceToGather.Id] += rand.Int63n(10) + rand.Int63n(100) + 5
+
+	return &character.GatherResourceResponse{Character: c, Inventory: c.Inventory}, nil
 }
